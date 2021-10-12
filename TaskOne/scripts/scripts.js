b33abd9cbbf4f53875ce4ccbbc6f68be109a6387
Nikita-Rasshivalov <71098405+Nikita-Rasshivalov@users.noreply.github.com>
2021-10-12 09:05:33 +0300

    Update scripts.js

--- TaskOne/scripts/scripts.js
+++ TaskOne/scripts/scripts.js
@@ -1,15 +1,31 @@
+/*function to change table properties
+params: (value) the received value by pressing the button
+*/
 function changeTable(value) {
     const table = document.getElementById('tableForChange');
     const buttons = document.querySelectorAll('[type="button"]');
+
     switch (value) {
         case 'style 1':
-            alert(table);
+            table.style.fontFamily = 'Arial';
+            table.style.fontSize = '25px';
+            for (var i = 0; i < buttons.length; i++) {
+                buttons[i].style.backgroundColor = "#ADCA94";
+            }
             break;
         case 'style 2':
-            alert('s2');
+            table.style.fontFamily = "Lucida Sans Unicode", "Lucida Grande";
+            table.style.fontSize = '14px';
+            for (var i = 0; i < buttons.length; i++) {
+                buttons[i].style.backgroundColor = "#b194ca";
+            }
             break;
         case 'style 3':
-            alert('s3');
+            table.style.fontFamily = "Cursive";
+            table.style.fontSize = '18px';
+            for (var i = 0; i < buttons.length; i++) {
+                buttons[i].style.backgroundColor = "#685b73";
+            }
             break;
         case '200px':
             table.style.height = '200px';
