740e551709c12c28c0f9ba5730da9693d5ae6a46
Nikita-Rasshivalov <71098405+Nikita-Rasshivalov@users.noreply.github.com>
2021-11-25 00:21:58 +0300

    Task five has been done

--- TaskFive/src/scripts/scripts.js
+++ TaskFive/src/scripts/scripts.js
@@ -1,79 +1,110 @@
-const calc = document.querySelector('.calc');
-const result =  document.querySelector('.result');
-const dotBtn = document.querySelector('.dot');
-const back = document.querySelector('.back').innerText;
-const JsonSender = document.querySelector('.JsonSender');
+//Constants
+const calc = document.querySelector(".calc");
+const result = document.querySelector(".result");
+const dotBtn = document.querySelector(".dot");
+const back = document.querySelector(".back").innerText;
+const jsonSender = document.querySelector(".JsonSender");
+const priority = document.querySelector("#priority");
+//Flag for checkbox
+let flag = true;
 //Array for sending json
 let JSonArray = [];
-
-if (document.querySelector('input[name=calcType]')) {
-    document.querySelectorAll('input[name=calcType]').forEach((elem) => {
-      elem.addEventListener("change", function(event) {
-        let item = event.target.value;
-        let intNum  =  'Integer';
-        dotBtn.style.pointerEvents = (item === intNum) ? 'none':'auto'
-      });
-    });
-  }
+//Check radio btn
+if (document.querySelector("input[name=calcType]")) {
+	document.querySelectorAll("input[name=calcType]").forEach((elem) => {
+		elem.addEventListener("change", function (event) {
+			let item = event.target.value;
+			let intNum = "Integer";
+			dotBtn.style.pointerEvents = item === intNum ? "none" : "auto";
+		});
+	});
+}
+//Check checkbox
+priority.addEventListener("change", () => {
+	if (!this.checked) flag = false;
+});
 
 // calculator functions processing
-calc.addEventListener('click',(event)=>{
-    let DoubleNum  =  'Double';
-    //Get radio values
-    let radioBtns = document.querySelector("input[name=calcType]:checked");
-    let radioBtnsValue = radioBtns ? radioBtns.value : "";
+calc.addEventListener("click", (event) => {
+	let DoubleNum = "Double";
+	//Get radio values
+	let radioBtns = document.querySelector("input[name=calcType]:checked");
+	let radioBtnsValue = radioBtns ? radioBtns.value : "";
 
-    if(!event.target.classList.contains('item__btn')) return
-    const value = event.target.innerText; 
-    try {
-        switch(value){
-            case 'AC':
-                result.innerText = '';
-                break;
-            case '=':
-                if(result.innerText.search(/[^0-9*/+-.()]/mi) != -1) return
-                JSonArray.push(result.innerText);
-                if( result.innerText != ''){
-                    result.innerText = (radioBtnsValue == DoubleNum) ? eval(result.innerText) : Math.floor(eval(result.innerText));
-                }
-                break;
-            case back:
-                result.innerText = result.innerText.substring(0,result.innerText.length-1)
-                break;
-            default:
-                result.innerText += value;
-        }
-      } catch(err) {
-            result.innerText = '';
-      }
+	if (!event.target.classList.contains("item__btn")) return;
+
+	const value = event.target.innerText;
+	try {
+		switch (value) {
+			case "AC":
+				result.innerText = "";
+				break;
+			case "=":
+				if (result.innerText.search(/[^0-9*/+-.()]/im) != -1) return;
+				JSonArray.push(result.innerText);
+
+				if (result.innerText != "")
+					if (flag == true) {
+						result.innerText =
+							radioBtnsValue == DoubleNum	? eval(result.innerText) : Math.floor(eval(result.innerText));
+					} else {
+						result.innerText = WithoutPriority(result.innerText);
+					}
+				break;
+			case back:
+				result.innerText = result.innerText.substring(
+					0,
+					result.innerText.length - 1
+				);
+				break;
+			default:
+				result.innerText += value;
+		}
+	} catch (err) {
+		result.innerText = "";
+	}
 });
 
+// Calc without priority
+let WithoutPriority = (result) => {
+	result = result.replace(/[{()}]/g, "");
+	let reForNumbers = /[0-9]/gi;
+	let reForSumbols = /[*/+-]/gi;
+	let numbers = result.match(reForNumbers);
+	let symbols = result.match(reForSumbols);
+	result = numbers[0];
+	for (let i = 0; i < symbols.length; i++) {
+		result = eval(result + symbols[i] + numbers[i + 1]);
+	}
+	return result;
+};
 
-//Json sender
-JsonSender.addEventListener('click',(event)=>{
-    const jsn = JsonSender.innerText ;
-    if(!event.target.classList.contains('JsonSender')) return
-    const value = event.target.innerText; 
-    try {
-        switch(value){
-            case jsn:
-                sendJson(JSonArray);
-                break;
-            default:
-                console.log("Json not sended")  
-        }
-      } catch(err) {
-            console.log(err)
-      }
+//Json sender btn
+jsonSender.addEventListener("click", (event) => {
+	const jsn = jsonSender.innerText;
+	if (!event.target.classList.contains("JsonSender")) return;
+	const value = event.target.innerText;
+	try {
+		switch (value) {
+			case jsn:
+				sendJson(JSonArray);
+				break;
+			default:
+				console.log("Json not sended");
+		}
+	} catch (err) {
+		console.log(err);
+	}
 });
 
-// Send json
-let sendJson = (arr)=>{
-    let jsonArray = JSON.parse(JSON.stringify(arr));
-    let xhr = new XMLHttpRequest();
-    let url = "url";
-    xhr.open("POST", url, true);
-    xhr.setRequestHeader("Content-Type", "application/json");
-    let data = JSON.stringify({expression:jsonArray });
-    xhr.send(data);
-}
+// Send request
+let sendJson = (arr) => {
+	let jsonArray = JSON.parse(JSON.stringify(arr));
+	let xhr = new XMLHttpRequest();
+	let url = "https://jsonplaceholder.typicode.com//";
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-Type", "application/json");
+	let data = JSON.stringify({ expressions: jsonArray });
+	//console.log(data)
+	xhr.send(data);
+};
