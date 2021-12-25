66aea91a19cc0c62fa6eaa14739f644ee509860e
Nikita-Rasshivalov <71098405+Nikita-Rasshivalov@users.noreply.github.com>
2021-12-25 23:26:15 +0300

    tasks 1 - 5 has been done

--- TaskSix/src/scripts/scripts.js
+++ TaskSix/src/scripts/scripts.js
@@ -1,10 +1,11 @@
 //#region fill the page with data
-
+/**
+ * variables for radios and checkboxs
+ */
 let pizzaBaseNames = [PuffBase.name, ThickBase.name, СalzoneBase.name, ThinBase.name]
 let productNames = [Meat.name, Chease.name, Sausage.name, Olives.name]
 let saucesNames = [Barbeku.name, SweetAndSour.name, Garlic.name, Teriyaki.name]
 let spicesNames = [Rosemary.name, Basilic.name, Oregano.name]
-
 let pizzaBaseContainer = document.querySelector('.pizzaBase-container');
 
 pizzaBaseNames.forEach(elem => {
@@ -102,8 +103,13 @@ if (document.querySelector(".pizzaBase-container")) {
     });
 }
 
-function setPizzaBase(base) {
-    switch (base) {
+/**
+ * Set pizza base.
+ * @param {item} baseName
+ * @returns {object} base
+ */
+function setPizzaBase(item) {
+    switch (item) {
         case "PuffBase":
             return new PuffBase();
         case "ThickBase":
@@ -117,192 +123,175 @@ function setPizzaBase(base) {
     }
 }
 
-//set products
+/**
+ * Remove ingredients.
+ * @param {arr} array
+ * @param {item} item
+ */
+function clearIngredient(arr, item) {
+    let currentIndex;
+    arr.forEach(function (entry, index) {
+        if (entry.name === item) currentIndex = index;
+    });
+    arr.splice(currentIndex, 1);
+}
+
+/**
+ * Add products
+ * @param {item} productName
+
+ */
+function addProducts(item) {
+    switch (item) {
+        case "Meat":
+            products.push(new Meat());
+            break;
+        case "Chease":
+            products.push(new Chease());
+            break;
+        case "Sausage":
+            products.push(new Sausage());
+            break;
+        case "Olives":
+            products.push(new Olives());
+            break;
+        default:
+            alert("Please choose a correct ingredient");
+    }
+}
+
+/**
+ * Remove products
+ * @param {item} productName
+ */
+function RemoveProducts(item) {
+    switch (item) {
+        case "Meat":
+            clearIngredient(products, item);
+            break;
+        case "Chease":
+            clearIngredient(products, item);
+            break;
+        case "Sausage":
+            clearIngredient(products, item);
+            break;
+        case "Olives":
+            clearIngredient(products, item);
+            break;
+        default:
+            alert("Please choose a correct ingredient");
+    }
+}
+
+//manage products
 if (document.querySelector(".products-container")) {
     document.querySelectorAll("input[name=products]").forEach((elem) => {
         elem.addEventListener("change", (event) => {
             let item = event.target.value;
             if (elem.checked) {
-                switch (item) {
-                    case "Meat":
-                        products.push(new Meat());
-                        break;
-                    case "Chease":
-                        products.push(new Chease());
-                        break;
-                    case "Sausage":
-                        products.push(new Sausage());
-                        break;
-                    case "Olives":
-                        products.push(new Olives());
-                        break;
-                    default:
-                        alert("Please choose a correct ingredient");
-                }
+                addProducts(item);
+            } else {
+                RemoveProducts(item)
             }
         });
     });
 }
 
-//clear products
-if (document.querySelector(".products-container")) {
-    document.querySelectorAll("input[name=products]").forEach((elem) => {
-        elem.addEventListener("change", (event) => {
-            let item = event.target.value;
-            let currentIndex;
-            if (!elem.checked) {
-                switch (item) {
-                    case "Meat":
-                        products.forEach(function (entry, index) {
-                            if (entry.name === item) currentIndex = index;
-                        });
-                        products.splice(currentIndex, 1);
-                        break;
-                    case "Chease":
-                        products.forEach(function (entry, index) {
-                            if (entry.name === item) currentIndex = index;
-                        });
-                        products.splice(currentIndex, 1);
-                        break;
-                    case "Sausage":
-                        products.forEach(function (entry, index) {
-                            if (entry.name === item) currentIndex = index;
-                        });
-                        products.splice(currentIndex, 1);
-                        break;
-                    case "Olives":
-                        products.forEach(function (entry, index) {
-                            if (entry.name === item) currentIndex = index;
-                        });
-                        products.splice(currentIndex, 1);
-                        break;
-                    default:
-                        alert("Please choose a correct ingredient");
-                }
-            }
-        });
-    });
+//add sauces
+function addSauces(item) {
+    switch (item) {
+        case "Barbeku":
+            sauces.push(new Barbeku());
+            break;
+        case "SweetAndSour":
+            sauces.push(new SweetAndSour());
+            break;
+        case "Garlic":
+            sauces.push(new Garlic());
+            break;
+        case "Teriyaki":
+            sauces.push(new Teriyaki());
+            break;
+        default:
+            alert("Please choose a correct ingredient");
+    }
+}
+//Remove products
+function RemoveSauces(item) {
+    switch (item) {
+        case "Barbeku":
+            clearIngredient(sauces, item);
+            break;
+        case "SweetAndSour":
+            clearIngredient(sauces, item);
+            break;
+        case "Garlic":
+            clearIngredient(sauces, item);
+            break;
+        case "Teriyaki":
+            clearIngredient(sauces, item);
+            break;
+        default:
+            alert("Please choose a correct ingredient");
+    }
 }
 
-//set sauces
+//manage sauces
 if (document.querySelector(".sauces-container")) {
     document.querySelectorAll("input[name=sauces]").forEach((elem) => {
         elem.addEventListener("change", (event) => {
             let item = event.target.value;
             if (elem.checked) {
-                switch (item) {
-                    case "Barbeku":
-                        sauces.push(new Barbeku());
-                        break;
-                    case "SweetAndSour":
-                        sauces.push(new SweetAndSour());
-                        break;
-                    case "Garlic":
-                        sauces.push(new Garlic());
-                        break;
-                    case "Teriyaki":
-                        sauces.push(new Teriyaki());
-                        break;
-                    default:
-                        alert("Please choose a correct ingredient");
-                }
-            }
-        });
-    });
-}
-//clear sauces
-if (document.querySelector(".sauces-container")) {
-    document.querySelectorAll("input[name=sauces]").forEach((elem) => {
-        elem.addEventListener("change", (event) => {
-            let item = event.target.value;
-            let currentIndex;
-            if (!elem.checked) {
-                switch (item) {
-                    case "Barbeku":
-                        sauces.forEach(function (entry, index) {
-                            if (entry.name === item) currentIndex = index;
-                        });
-                        sauces.splice(currentIndex, 1);
-                        break;
-                    case "SweetAndSour":
-                        sauces.forEach(function (entry, index) {
-                            if (entry.name === item) currentIndex = index;
-                        });
-                        sauces.splice(currentIndex, 1);
-                        break;
-                    case "Garlic":
-                        sauces.forEach(function (entry, index) {
-                            if (entry.name === item) currentIndex = index;
-                        });
-                        sauces.splice(currentIndex, 1);
-                        break;
-                    case "Teriyaki":
-                        sauces.forEach(function (entry, index) {
-                            if (entry.name === item) currentIndex = index;
-                        });
-                        sauces.splice(currentIndex, 1);
-                        break;
-                    default:
-                        alert("Please choose a correct ingredient");
-                }
+                addSauces(item);
+            } else {
+                RemoveSauces(item);
             }
         });
     });
 }
 
-//set species
+//add species
+function addSpecies(item) {
+    switch (item) {
+        case "Oregano":
+            spices.push(new Oregano());
+            break;
+        case "Basilic":
+            spices.push(new Basilic());
+            break;
+        case "Rosemary":
+            spices.push(new Rosemary());
+            break;
+        default:
+            alert("Please choose a correct ingredient");
+    }
+}
+//Remove species
+function RemoveSpecies(item) {
+    switch (item) {
+        case "Oregano":
+            clearIngredient(spices, item);
+            break;
+        case "Basilic":
+            clearIngredient(spices, item);
+            break;
+        case "Rosemary":
+            clearIngredient(spices, item);
+            break;
+        default:
+            alert("Please choose a correct ingredient");
+    }
+}
+
+//manage species
 if (document.querySelector(".spices-container")) {
     document.querySelectorAll("input[name=spices]").forEach((elem) => {
         elem.addEventListener("change", (event) => {
             let item = event.target.value;
             if (elem.checked) {
-                switch (item) {
-                    case "Oregano":
-                        spices.push(new Oregano());
-                        break;
-                    case "Basilic":
-                        spices.push(new Basilic());
-                        break;
-                    case "Rosemary":
-                        spices.push(new Rosemary());
-                        break;
-                    default:
-                        alert("Please choose a correct ingredient");
-                }
-            }
-        });
-    });
-}
-
-// clear spices
-if (document.querySelector(".spices-container")) {
-    document.querySelectorAll("input[name=spices]").forEach((elem) => {
-        elem.addEventListener("change", (event) => {
-            let item = event.target.value;
-            let currentIndex;
-            if (!elem.checked) {
-                switch (item) {
-                    case "Oregano":
-                        spices.forEach(function (entry, index) {
-                            if (entry.name === item) currentIndex = index;
-                        });
-                        spices.splice(currentIndex, 1);
-                        break;
-                    case "Basilic":
-                        spices.forEach(function (entry, index) {
-                            if (entry.name === item) currentIndex = index;
-                        });
-                        spices.splice(currentIndex, 1);
-                        break;
-                    case "Rosemary":
-                        spices.forEach(function (entry, index) {
-                            if (entry.name === item) currentIndex = index;
-                        });
-                        spices.splice(currentIndex, 1);
-                        break;
-                    default:
-                        alert("Please choose a correct ingredient");
-                }
+                addSpecies(item);
+            } else {
+                RemoveSpecies(item);
             }
         });
     });
@@ -311,28 +300,68 @@ if (document.querySelector(".spices-container")) {
 
 //#region calculation 
 //variables
-let pizza = new Pizza(pizzaBase, products, sauces, spices);
 
 const resultSum = document.querySelector(".result-sum");
 const resultCal = document.querySelector(".result-cal");
+const K1 = 80;
+const K2 = 120;
 
-let pizzaBaseSum = 0;
-let pizzaBaseCal = 0;
-let productsSum = 0;
-let productsCal = 0;
-let sausesSum = 0;
-let sausesCal = 0;
-let speciesSum = 0;
-let speciesCal = 0;
 
-document.querySelectorAll("input[name=sauces]").forEach((elem) => {
-    elem.addEventListener("change", () => {
-        pizza.sauces.forEach(elem => {
-            sausesSum += elem.price
-            sausesCal += elem.calories
-        });
+/**
+ * get price.
+ * @param {arr} array
+ * @returns {sumPrice}
+ */
+function getPrice(arr) {
+    let sumPrice = 0;
+    arr.forEach(elem => {
+        sumPrice += elem.price
+    })
+    return sumPrice;
+}
+
+/**
+ * get calories.
+ * @param {arr} array
+ * @returns {sumPrice}
+ */
+function getCalories(arr) {
+    let sumCal = 0;
+    arr.forEach(elem => {
+        sumCal += elem.calories
+    })
+    return sumCal;
+}
+
+//set calories and price to page
+document.querySelectorAll('input[name=pizzaBase],input[name=sauces],input[name=products],input[name=spices]').forEach(elem => {
+    elem.addEventListener('change', event => {
+
+        let spicesPrice = getPrice(spices);
+        let productsPrice = getPrice(products);
+        let saucesPrice = getPrice(sauces);
+        let basePrice = pizzaBase ? pizzaBase.price : 0;
+        let generalPrice = spicesPrice + productsPrice + saucesPrice + basePrice;
+
+        // fulfillment of point 4 of the task
+        //console.log(generalPrice);
+        if (generalPrice < K1) {
+            generalPrice = generalPrice + (generalPrice * 0.2)
+        }
+        else if (generalPrice >= K1 && generalPrice <= K2) {
+            generalPrice = generalPrice + (generalPrice * 0.15)
+        } else {
+            generalPrice = generalPrice + (generalPrice * 0.1)
+        }
+
+        resultSum.innerHTML = generalPrice + " rub";
+
+        let spicesCal = getCalories(spices);
+        let productsCal = getCalories(products);
+        let saucesCal = getCalories(sauces);
+        let baseCal = pizzaBase ? pizzaBase.calories : 0;
+        let generalCal = spicesCal + productsCal + saucesCal + baseCal;
+        resultCal.innerHTML = generalCal + " kcal";
     });
 });
-
-
 //#endregion
