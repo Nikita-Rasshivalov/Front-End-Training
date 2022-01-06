218a9726fa635888ed23bc23eacb7420eeba994a
Nikita-Rasshivalov <71098405+Nikita-Rasshivalov@users.noreply.github.com>
2022-01-06 22:25:12 +0300

    initial commit

---
+++ TaskSeven/src/client/scripts/scripts.js
@@ -0,0 +1,16 @@
+window.addEventListener("load",()=>{
+    const canvas = document.querySelector("canvas");
+    const ctx = canvas.getContext("2d");
+    
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+
+
+
+
+});
+
+
+
+
+
